e26abe6b704ab70c2048a3d1cce9b48da5979e00
Morgan Bradley <mbradleystylist@gmail.com>
2017-11-16 18:35:24 +0000

    initialized Firebase on script.js file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+ // Initialize Firebase
+ /* global firebase */
 
+ var config = {
+     apiKey: "AIzaSyA04Li4Y2BJ6O54i1Tm3VgZx6XX1JibX1c",
+     authDomain: "polimotions.firebaseapp.com",
+     databaseURL: "https://polimotions.firebaseio.com",
+     projectId: "polimotions",
+     storageBucket: "polimotions.appspot.com",
+     messagingSenderId: "920170050300"
+ };
+
+ firebase.initializeApp(config);
+ 
