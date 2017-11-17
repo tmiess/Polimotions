8f99c53c79229b0aa1d36f4a1816b1d5e3a724ec
Morgan Bradley <mbradleystylist@gmail.com>
2017-11-17 15:37:09 +0000

    moved javascript to app.js, moved script tag to head in html

--- script.js
+++ script.js
@@ -11,4 +11,19 @@
  };
 
  firebase.initializeApp(config);
+
+ var imageURL = "http://cdn.history.com/sites/2/2013/11/George_Washington-AB.jpeg";
+
+ var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect?api_key=AKof96jqIYUIqbmI2TaF3-AJcURETpor&api_secret=WbNCep4Ml1Ad_wTiItDTq7QhTEskPUYT&image_url=" + imageURL + "&return_attributes=gender,age,emotion";
+
+ console.log(imageURL);
+
+ $.ajax({ url: queryURL, method: "GET" })
+
+     .done(function(response) {
+         var results = response.data;
+
+         console.log(results);
+
+     });
  
