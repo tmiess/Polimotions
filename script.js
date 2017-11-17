e733404219a3d248f6db40c407c9da1a6092abcb
Morgan Bradley <mbradleystylist@gmail.com>
2017-11-17 18:27:54 +0000

    updated js for face++

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 //IMAGE SEARCH
 $(document).on("mouseover", function() {
-    $("img").addClass('draggable="true"');
+    $("img").attr("draggable", "true");
+    $("img").attr("ondragstart", "drag(event)");
+    console.log("Dragged");
+});
+$(document).on("mousedown", function() {
+    $("img").attr("id", "drag1");
+    console.log("where is the picture")
+
 });
 
-// function allowDrop(ev) {
-//     ev.preventDefault();
-// }
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
-// function drag(ev) {
-//     ev.dataTransfer.setData("text", ev.target.id);
-// }
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log("Hey");
+}
 
-// function drop(ev) {
-//     ev.preventDefault();
-//     var data = ev.dataTransfer.getData("text");
-//     ev.target.appendChild(document.getElementById(data));
-// }
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    console.log("Hi");
+}
+
 
 // Initialize Firebase
 /* global firebase */
