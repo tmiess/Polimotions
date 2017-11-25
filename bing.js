9c43ecba4ec1e2eef2815054562a55da23c19eef
Katie Atwell <katieatwell@outlook.com>
2017-11-25 20:07:36 +0000

    testing table/gallery for images

--- bing.js
+++ bing.js
@@ -1,37 +1,38 @@
  //Image Search
  $("#run-search").on("click", function() {
 
-     event.preventDefault();
+  event.preventDefault();
 
-     var imageSearch = $("#searchTerm").val().trim();
-     console.log("Your search is:" + imageSearch);
+  var imageSearch = $("#searchTerm").val().trim();
+  console.log("Your search is:" + imageSearch);
 
-     var queryURL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=" + imageSearch + "&count=10";
+  var queryURL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=" + imageSearch + "&count=9";
 
-     $.ajax({
-             url: queryURL,
-             beforeSend: function(xhrObj) {
-                 // Request headers
-                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "b8927aeb88e54d32a3c7bba435c9763e"); //replace value with your own key
-             },
-             method: "GET"
-         })
+  $.ajax({
+    url: queryURL,
+    beforeSend: function(xhrObj) {
+     // Request headers
+     xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "b8927aeb88e54d32a3c7bba435c9763e"); //replace value with your own key
+    },
+    method: "GET"
+   })
 
-         .done(function(response) {
-             console.log(response);
+   .done(function(response) {
+    console.log(response);
 
-             var results = response.value;
-             console.log(results);
+    var results = response.value;
+    console.log(results);
 
-             for (var j = 0; j < results.length; j++) {
+    for (var h = 0; h < results.length; h++) {
+     var topicImage = $("<img>");
+     topicImage.attr("src", results[h].contentUrl);
+     console.log(topicImage);
 
-                 var topicImage = $("<img>");
+     $(".images").append(topicImage);
 
-                 topicImage.attr("src", results[j].contentUrl);
+    }
 
-                 $(".images").append(topicImage);
-             }
 
-         });
+   });
  });
  
