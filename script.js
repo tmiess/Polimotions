b3a3327d3710d02fa64647bb5ac2816c7d2dfacc
Morgan Bradley <mbradleystylist@gmail.com>
2017-11-26 23:43:34 +0000

    sideNav

--- script.js
+++ script.js
@@ -1,184 +1,213 @@
-//Global Variables
-
-var imageLink;
-
 $(document).ready(function() {
-    var image = $("<img>").attr("src", "https://hyperallergic.com/wp-content/uploads/2016/12/whitehouseoldestphoto-720x527.jpg");
-    var imagePlace = $("#div1");
 
-    imagePlace.append(image);
+ //SIDENAV
+ $('.button-collapse').sideNav('show');
+ $('.button-collapse').sideNav('hide');
+ $('.button-collapse').sideNav('destroy');
 
-    //IMAGE DROPBOX
-    var dropbox = document.getElementById("dropbox");
-    dropbox.addEventListener("dragenter", noopHandler, false);
-    dropbox.addEventListener("dragexit", noopHandler, false);
-    dropbox.addEventListener("dragover", noopHandler, false);
-    dropbox.addEventListener("drop", drop, false);
-
-    function noopHandler(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        console.log("Handler working")
-    }
-
-    function drop(evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        imageLink = evt.dataTransfer.getData('URL');
-        console.log(imageLink);
-        $("#dropbox").prepend('<img src="' + imageLink + '">');
-        console.log('<img src="' + imageLink + '">');
-    }
-    //DRAG GEORGE
-    // $(document).on("mousedown", function() {
-    //     var image = $('img').addClass("draggable");
-    //     console.log(image);
-    // });
-
-    // $(document).on("mousedown", function() {
-    //     $(".draggable").draggable();
-    //     console.log("Am I draggable?");
-    // });
-
-    $("#dragMe").draggable();
-
-    //Clear Image Div
-
-    $("#resetDiv").on("click", function() {
-        $("#dropbox").empty();
-    });
+ $(".button-collapse").sideNav();
+ $('.collapsible').collapsible();
+ $('.button-collapse').sideNav({
+  menuWidth: 300, // Default is 300
+  edge: 'left', // Choose the horizontal origin
+  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  draggable: true, // Choose whether you can drag to open on touch screens,
+ });
 
 
-    //IMAGE SEARCH
-    // $(document).on("mouseover", function() {
-    //     $("img").attr("draggable", "true");
-    //     $("img").attr("ondragstart", "drag(event)");
-    //     console.log("Dragged");
-    // });
-    // $(document).on("mousedown", function() {
-    //     $("img").attr("id", "drag1");
-    //     console.log("where is the picture")
-    // });
-
-    // function allowDrop(ev) {
-    //     ev.preventDefault();
-    // }
-
-    // function drag(ev) {
-    //     ev.dataTransfer.setData("text", ev.target.id);
-    //     console.log("Hey");
-    // }
 
 
-    // function drop(ev) {
-    //     ev.preventDefault();
-    //     var data = ev.dataTransfer.getData("URL");
-    //     ev.target.appendChild(document.getElementById(data));
-    //     console.log("Hi");
-    // }
 
 
-    // Initialize Firebase
-    /* global firebase */
+ //Global Variables
 
-    var config = {
-        apiKey: "AIzaSyA04Li4Y2BJ6O54i1Tm3VgZx6XX1JibX1c",
-        authDomain: "polimotions.firebaseapp.com",
-        databaseURL: "https://polimotions.firebaseio.com",
-        projectId: "polimotions",
-        storageBucket: "polimotions.appspot.com",
-        messagingSenderId: "920170050300"
-    };
-
-    firebase.initializeApp(config);
+ var imageLink;
 
 
-    var db = firebase.database();
-    $("#run-search").on("click", function(event) {
-        event.preventDefault();
+ //IMAGE DROPBOX
+ var dropbox = document.getElementById("dropbox");
+ dropbox.addEventListener("dragenter", noopHandler, false);
+ dropbox.addEventListener("dragexit", noopHandler, false);
+ dropbox.addEventListener("dragover", noopHandler, false);
+ dropbox.addEventListener("drop", drop, false);
 
-        var recentSearch = $("#searchTerm").val().trim();
-        $("#name").text(recentSearch);
-        db.ref().push({
-            recentSearch: recentSearch,
-        });
+ function noopHandler(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  console.log("Handler working")
+ }
 
-        console.log(recentSearch);
+ function drop(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  imageLink = evt.dataTransfer.getData('URL');
+  console.log(imageLink);
+  $("#dropbox").prepend('<img src="' + imageLink + '">');
+  console.log('<img src="' + imageLink + '">');
+ }
+ //DRAG GEORGE
+ // $(document).on("mousedown", function() {
+ //     var image = $('img').addClass("draggable");
+ //     console.log(image);
+ // });
 
-        $("#searchTerm").val("");
-    });
+ // $(document).on("mousedown", function() {
+ //     $(".draggable").draggable();
+ //     console.log("Am I draggable?");
+ // });
 
-    //FACE++ API
+ $("#dragMe").draggable();
+
+ //Clear Image Div
+
+ $("#resetDiv").on("click", function() {
+  $("#dropbox").empty();
+  $("#analysisDiv").val("");
+  $("#age").val("");
+  $("#sad").val("");
+  $("#neutral").val("");
+  $("#disgust").val("");
+  $("#anger").val("");
+  $("#surprise").val("");
+  $("#div1").empty();
+ });
 
 
-    db.ref().limitToLast(6).on("child_added", function(childSnapshot, prevChildKey) {
-        console.log(childSnapshot.val());
+ //IMAGE SEARCH
+ // $(document).on("mouseover", function() {
+ //     $("img").attr("draggable", "true");
+ //     $("img").attr("ondragstart", "drag(event)");
+ //     console.log("Dragged");
+ // });
+ // $(document).on("mousedown", function() {
+ //     $("img").attr("id", "drag1");
+ //     console.log("where is the picture")
+ // });
 
-        var recentSearch = childSnapshot.val().recentSearch;
-        var newDiv = $('<div>');
-        newDiv.append($('<p>').text(recentSearch));
+ // function allowDrop(ev) {
+ //     ev.preventDefault();
+ // }
 
-        $("#searches").prepend(newDiv);
-    });
+ // function drag(ev) {
+ //     ev.dataTransfer.setData("text", ev.target.id);
+ //     console.log("Hey");
+ // }
 
 
-    //FACE++ API
-    $("#analysisButton").on("click", function() {
-        $("#analysisDiv").val("");
-        $("#age").val("");
-        $("#sad").val("");
-        $("#neutral").val("");
-        $("#disgust").val("");
-        $("#anger").val("");
-        $("#surprise").val("");
-        $("#div1").empty();
+ // function drop(ev) {
+ //     ev.preventDefault();
+ //     var data = ev.dataTransfer.getData("URL");
+ //     ev.target.appendChild(document.getElementById(data));
+ //     console.log("Hi");
+ // }
 
-        var imageURL = "https://cdn.history.com/sites/2/2013/11/George_Washington-AB.jpeg";
 
-        console.log(imageURL);
+ // Initialize Firebase
+ /* global firebase */
 
-        var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect";
+ var config = {
+  apiKey: "AIzaSyA04Li4Y2BJ6O54i1Tm3VgZx6XX1JibX1c",
+  authDomain: "polimotions.firebaseapp.com",
+  databaseURL: "https://polimotions.firebaseio.com",
+  projectId: "polimotions",
+  storageBucket: "polimotions.appspot.com",
+  messagingSenderId: "920170050300"
+ };
 
-        var params = {
-            "api_key": "AKof96jqIYUIqbmI2TaF3-AJcURETpor",
-            "api_secret": "WbNCep4Ml1Ad_wTiItDTq7QhTEskPUYT",
-            "image_url": imageURL,
-            "return_attributes": "gender,age,emotion",
-        };
+ firebase.initializeApp(config);
 
-        $.ajax({ url: queryURL, method: "POST", data: params })
 
-            .done(function(response) {
-                var results = response;
-                console.log(results);
+ var db = firebase.database();
+ $("#run-search").on("click", function(event) {
+  event.preventDefault();
 
-                var analysis = $("#analysisDiv");
+  var recentSearch = $("#searchTerm").val().trim();
+  $("#name").text(recentSearch);
+  db.ref().push({
+   recentSearch: recentSearch,
+  });
 
-                // variables to catch emotions
-                var sadness = results.faces[0].attributes.emotion.sadness;
-                var neutral = results.faces[0].attributes.emotion.neutral;
-                var disgust = results.faces[0].attributes.emotion.disgust;
-                var anger = results.faces[0].attributes.emotion.anger;
-                var surprise = results.faces[0].attributes.emotion.surprise;
+  console.log(recentSearch);
 
-                // variables to catch age and gender
-                var age = results.faces[0].attributes.age.value;
-                var gender = results.faces[0].attributes.gender.value;
+  $("#searchTerm").val("");
+ });
 
-                var image = $("<img>").attr("src", imageURL);
-                var imagePlace = $("#div1");
+ //FACE++ API
 
-                imagePlace.append(image);
 
-                // results appear in html table
-                $("#age").text(age);
-                $("#gender").text(gender);
-                $("#sad").text(sadness);
-                $("#neutral").text(neutral);
-                $("#disgust").text(disgust);
-                $("#anger").text(anger);
-                $("#surprise").text(surprise);
+ db.ref().limitToLast(6).on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
 
-            });
-    });
+  var recentSearch = childSnapshot.val().recentSearch;
+  var newDiv = $('<div>');
+  newDiv.append($('<p>').text(recentSearch));
+
+  $("#searches").prepend(newDiv);
+ });
+
+
+ //FACE++ API
+ $("#analysisButton").on("click", function() {
+
+
+  var imageURL = "https://cdn.history.com/sites/2/2013/11/George_Washington-AB.jpeg";
+
+  console.log(imageURL);
+
+  var queryURL = "https://api-us.faceplusplus.com/facepp/v3/detect";
+
+  var params = {
+   "api_key": "AKof96jqIYUIqbmI2TaF3-AJcURETpor",
+   "api_secret": "WbNCep4Ml1Ad_wTiItDTq7QhTEskPUYT",
+   "image_url": imageURL,
+   "return_attributes": "gender,age,emotion",
+  };
+
+  $.ajax({ url: queryURL, method: "POST", data: params })
+
+   .done(function(response) {
+    var results = response;
+    console.log(results);
+
+    // variables to catch emotions
+    var sadness = results.faces[0].attributes.emotion.sadness;
+    var neutral = results.faces[0].attributes.emotion.neutral;
+    var disgust = results.faces[0].attributes.emotion.disgust;
+    var anger = results.faces[0].attributes.emotion.anger;
+    var surprise = results.faces[0].attributes.emotion.surprise;
+    var happiness = results.faces[0].attributes.emotion.happiness;
+    var fear = results.faces[0].attributes.emotion.fear;
+
+    // variables to catch age and gender
+    var age = results.faces[0].attributes.age.value;
+    var gender = results.faces[0].attributes.gender.value;
+
+    // results appear in html table
+    $("#age").text(age);
+    $("#gender").text(gender);
+    $("#sad").text(sadness);
+    $("#neutral").text(neutral);
+    $("#disgust").text(disgust);
+    $("#anger").text(anger);
+    $("#surprise").text(surprise);
+    $("#happiness").text(happiness);
+    $("#fear").text(fear);
+
+   });
+ });
+
+ //IMAGE CAROUSEL
+ $(document).ready(function() {
+  $('.carousel').carousel();
+ });
+
+ // var carouselImage = $(".carousel-item").attr("src");
+
+ // $(".carousel-item").on("click", function() {
+ //  $("#dropbox").prepend(carouselImage);
+ // });
+
+ // MOMENT JS FOR CURRENT DAY AND TIME UNDER NAV
+ /*global moment*/
+ $("#toptext").text(moment().format('MMMM Do, YYYY'));
 });
